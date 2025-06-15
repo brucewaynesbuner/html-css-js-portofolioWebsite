b14cf1b3c7c3e2ac56e1382cdf98e43b4eab086f
brucewaynesbuner <masariratapiwa@gmail.com>
2025-06-15 16:22:08 +0200

    changes made to the website:

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 function toggleMenu() {
-  const menuLinks = document.querySelector('.menu-links');
-  menuLinks.classList.toggle('show');
-}
-
-let currentIndex = 0;
-
-function moveCarousel(direction) {
-  const track = document.querySelector('.carousel-track');
-  const items = document.querySelectorAll('.skill-card');
-  const itemWidth = items[0].offsetWidth;
-
-  currentIndex += direction;
-
-  if (currentIndex < 0) {
-    currentIndex = items.length - 1;
-  } else if (currentIndex >= items.length) {
-    currentIndex = 0;
-  }
-
-  track.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
